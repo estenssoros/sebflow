2349cfe77c9d34c79b10a91f721df7747d1d55b0
Sebastian Estenssoro <estenssoros@Sebastians-MacBook-Pro.local>
2017-12-12 20:42:13 -0700

    first commit

---
+++ sebflow/bin/sebflow.py
@@ -0,0 +1,8 @@
+#!/usr/bin env python
+# -*- coding: utf-8 -*-
+
+from sebflow.bin.cli import CLIFactory
+
+if __name__ == '__main__':
+    parser = arparse.ArgumentParser()
+    parser.add_argument()
---
+++ sebflow/dag.py
@@ -0,0 +1,2 @@
+class DAG(object):
+    def __init__(self, name):
