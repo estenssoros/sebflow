34f1203c19d8dcd87c5f69b523d5162aeee8aa5c
Sebastian Estenssoro <estenssoros@Sebastians-MacBook-Pro.local>
2018-01-02 20:21:10 -0700

    rethinking dag table

--- sebflow/utils/db.py
+++ sebflow/utils/db.py
@@ -1,8 +1,18 @@
-from sebflow import settings
+import os
+import sys
+
 import MySQLdb
+from sebflow import settings
+
 
 
 def init_db():
+
+    dags = [f for f in os.listdir(settings.DAG_FOLDER) if f.endswith('.py')]
+    if not dags:
+        print('No dags present in %s' % dag_folder)
+        sys.exit(1)
+
     conn = MySQLdb.connect(**settings.DB_CREDS)
     curs = conn.cursor()
 
@@ -12,7 +22,10 @@ def init_db():
     curs.execute('''
     CREATE TABLE dags (
         id SERIAL,
-        name VARCHAR(50),
+        description TEXT,
+        schedule_interval VARCHAR(10),
+        start_date DATE DEFAULT CURRENT_TIMESTAMP,
+        end_date DATE,
         last_run DATETIME DEFAULT NULL,
         success TINYINT(1)
     )
