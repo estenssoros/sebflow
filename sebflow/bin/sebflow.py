09c563188bfa02624f18f78c655670a1fc47333b
Sebastian Estenssoro <estenssoros@Sebastians-MacBook-Pro.local>
2018-01-02 18:47:08 -0700

    renamed, made executable

--- sebflow/bin/sebflow.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin env python
-# -*- coding: utf-8 -*-
-
-from sebflow.bin.cli import CLIFactory
-
-if __name__ == '__main__':
-    parser = arparse.ArgumentParser()
-    parser.add_argument()
