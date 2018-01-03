6827ba77cca02028ef94a1fb320909d927964f4e
Sebastian Estenssoro <estenssoros@Sebastians-MacBook-Pro.local>
2018-01-02 18:48:37 -0700

    coded, made executable

--- sebflow/bin/cli.py
+++ sebflow/bin/cli.py
@@ -1,1 +1,53 @@
-class CLIFactory(object)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from collections import namedtuple
+from sebflow import settings
+from utils import db as db_utils
+
+Arg = namedtuple('Arg', ['flags', 'help', 'action', 'default', 'nargs', 'type', 'choices', 'metavar'])
+Arg.__new__.__defaults__ = (None, None, None, None, None, None, None)
+
+
+def initdb(args):
+    print("Initializing DB...")
+    db_utils.init_db()
+    print("Done.")
+
+
+class CLIFactory(object):
+    subparsers = (
+        {
+            'func': initdb,
+            'help': 'initalize the database',
+            'args': tuple()
+        },
+    )
+    subparsers_dict = {sp['func'].__name__: sp for sp in subparsers}
+    dag_subparsers = ('list_tasks', 'backfill', 'test', 'run', 'pause', 'unpause')
+
+    @classmethod
+    def get_parser(cls, dag_parser=False):
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers(help='sub-command help', dest='subcommand')
+        subparsers.required = True
+
+        subparser_list = cls.dag_subparsers if dag_parser else cls.subparsers_dict.keys()
+        for sub in subparser_list:
+            sub = cls.subparsers_dict[sub]
+            sp = subparsers.add_parser(sub['func'].__name__, help=sub['help'])
+            for arg in sub['args']:
+                if 'dag_id' in arg and dag_parser:
+                    continue
+                arg = cls.args[arg]
+                kwargs = {
+                    f: getattr(arg, f)
+                    for f in arg._fields if f != 'flags' and getattr(arg, f)}
+                sp.add_argument(*arg.flags, **kwargs)
+            sp.set_defaults(func=sub['func'])
+        return parser
+
+
+def get_parser():
+    return CLIFactory.get_parser()
