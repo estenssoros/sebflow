b8bf73e825630e6a34e4ecb440533dff29b417d9
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-10 16:00:10 -0700

    get fernet key and Connection model

--- sebflow/models.py
+++ sebflow/models.py
@@ -1,20 +1,42 @@
-from sqlalchemy import (Boolean, Column,
-                        Integer, String, func)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_utc import UtcDateTime
+import json
+from urllib.parse import urlparse
 
-from sebflow import timezone
+from colorama import init
+from sebflow import configuration, timezone
 # from sebflow.utils.decorators import apply_defaults
 from sebflow.exceptions import SebflowException
-from sebflow.utils.log.logging_mixin import LoggingMixin
 from sebflow.state import State
 from sebflow.utils.db import get_or_create, provide_session
-from colorama import init
+from sebflow.utils.log.logging_mixin import LoggingMixin
+from sqlalchemy import Boolean, Column, Integer, String, func
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import synonym
+from sqlalchemy_utc import UtcDateTime
 from termcolor import colored
 
 init()
 
 Base = declarative_base()
+
+
+def get_fernet():
+    """
+    Deferred load of Fernet key.
+
+    This function could fail either because Cryptography is not installed
+    or because the Fernet key is invalid.
+
+    :return: Fernet object
+    :raises: AirflowException if there's a problem trying to load Fernet
+    """
+    try:
+        from cryptography.fernet import Fernet
+    except:
+        raise SebflowException('Failed to import Fernet, it may not be installed')
+    try:
+        return Fernet(configuration.get('core', 'FERNET_KEY').encode('utf-8'))
+    except ValueError as ve:
+        raise SebflowException("Could not create Fernet object: {}".format(ve))
 
 
 class DagModel(Base):
@@ -251,8 +273,8 @@ class BaseOperator(LoggingMixin):
 
     def __repr__(self):
         state = colored(self.state, State.color(self.state))
-        if self.state==State.FAILED:
-            return "<Task({0}): {1}> [{2}] - {3}".format(self.__class__.__name__, self.task_id, state,self.msg)
+        if self.state == State.FAILED:
+            return "<Task({0}): {1}> [{2}] - {3}".format(self.__class__.__name__, self.task_id, state, self.msg)
         else:
             return "<Task({0}): {1}> [{2}]".format(self.__class__.__name__, self.task_id, state)
 
@@ -319,3 +341,186 @@ class Task(Base):
     end_date = Column(UtcDateTime)
     hostname = Column(String(50))
     unixname = Column(String(50))
+
+
+class Connection(Base, LoggingMixin):
+    __tablename__ = "connection"
+    id = Column(Integer(), primary_key=True)
+    conn_id = Column(String(50))
+    conn_type = Column(String(100))
+    host = Column(String(100))
+    schema = Column(String(100))
+    login = Column(String(100))
+    _password = Column('password', String(100))
+    port = Column(Integer())
+    is_encrypted = Column(Boolean, unique=False, default=False)
+    is_extra_encrypted = Column(Boolean, unique=False, default=False)
+    _extra = Column('extra', String(100))
+
+    _types = [
+        ('ftp', 'FTP',),
+        ('postgres', 'Postgres',),
+        ('oracle', 'Oracle',),
+        ('s3', 'S3',),
+        ('sqlite', 'Sqlite',),
+        ('mssql', 'Microsoft SQL Server'),
+    ]
+
+    def __init__(
+            self, conn_id=None, conn_type=None,
+            host=None, login=None, password=None,
+            schema=None, port=None, extra=None,
+            uri=None):
+        self.conn_id = conn_id
+        if uri:
+            self.parse_from_uri(uri)
+        else:
+            self.conn_type = conn_type
+            self.host = host
+            self.login = login
+            self.password = password
+            self.schema = schema
+            self.port = port
+            self.extra = extra
+
+    def parse_from_uri(self, uri):
+        temp_uri = urlparse(uri)
+        hostname = temp_uri.hostname or ''
+        if '%2f' in hostname:
+            hostname = hostname.replace('%2f', '/').replace('%2F', '/')
+        conn_type = temp_uri.scheme
+        if conn_type == 'postgresql':
+            conn_type = 'postgres'
+        self.conn_type = conn_type
+        self.host = hostname
+        self.schema = temp_uri.path[1:]
+        self.login = temp_uri.username
+        self.password = temp_uri.password
+        self.port = temp_uri.port
+
+    def get_password(self):
+        if self._password and self.is_encrypted:
+            try:
+                fernet = get_fernet()
+            except:
+                raise SebflowException(
+                    "Can't decrypt encrypted password for login={}, \
+                    FERNET_KEY configuration is missing".format(self.login))
+            return fernet.decrypt(bytes(self._password, 'utf-8')).decode()
+        else:
+            return self._password
+
+    def set_password(self, value):
+        if value:
+            try:
+                fernet = get_fernet()
+                self._password = fernet.encrypt(bytes(value, 'utf-8')).decode()
+                self.is_encrypted = True
+            except SebflowException:
+                self.log.exception("Failed to load fernet while encrypting value, "
+                                   "using non-encrypted value.")
+                self._password = value
+                self.is_encrypted = False
+
+    @declared_attr
+    def password(cls):
+        return synonym('_password',
+                       descriptor=property(cls.get_password, cls.set_password))
+
+    def get_extra(self):
+        if self._extra and self.is_extra_encrypted:
+            try:
+                fernet = get_fernet()
+            except:
+                raise SebflowException(
+                    "Can't decrypt `extra` params for login={},\
+                    FERNET_KEY configuration is missing".format(self.login))
+            return fernet.decrypt(bytes(self._extra, 'utf-8')).decode()
+        else:
+            return self._extra
+
+    def set_extra(self, value):
+        if value:
+            try:
+                fernet = get_fernet()
+                self._extra = fernet.encrypt(bytes(value, 'utf-8')).decode()
+                self.is_extra_encrypted = True
+            except SebflowException:
+                self.log.exception("Failed to load fernet while encrypting value, "
+                                   "using non-encrypted value.")
+                self._extra = value
+                self.is_extra_encrypted = False
+
+    @declared_attr
+    def extra(cls):
+        return synonym('_extra',
+                       descriptor=property(cls.get_extra, cls.set_extra))
+
+    def get_hook(self):
+        try:
+            if self.conn_type == 'mysql':
+                from airflow.hooks.mysql_hook import MySqlHook
+                return MySqlHook(mysql_conn_id=self.conn_id)
+            elif self.conn_type == 'google_cloud_platform':
+                from airflow.contrib.hooks.bigquery_hook import BigQueryHook
+                return BigQueryHook(bigquery_conn_id=self.conn_id)
+            elif self.conn_type == 'postgres':
+                from airflow.hooks.postgres_hook import PostgresHook
+                return PostgresHook(postgres_conn_id=self.conn_id)
+            elif self.conn_type == 'hive_cli':
+                from airflow.hooks.hive_hooks import HiveCliHook
+                return HiveCliHook(hive_cli_conn_id=self.conn_id)
+            elif self.conn_type == 'presto':
+                from airflow.hooks.presto_hook import PrestoHook
+                return PrestoHook(presto_conn_id=self.conn_id)
+            elif self.conn_type == 'hiveserver2':
+                from airflow.hooks.hive_hooks import HiveServer2Hook
+                return HiveServer2Hook(hiveserver2_conn_id=self.conn_id)
+            elif self.conn_type == 'sqlite':
+                from airflow.hooks.sqlite_hook import SqliteHook
+                return SqliteHook(sqlite_conn_id=self.conn_id)
+            elif self.conn_type == 'jdbc':
+                from airflow.hooks.jdbc_hook import JdbcHook
+                return JdbcHook(jdbc_conn_id=self.conn_id)
+            elif self.conn_type == 'mssql':
+                from airflow.hooks.mssql_hook import MsSqlHook
+                return MsSqlHook(mssql_conn_id=self.conn_id)
+            elif self.conn_type == 'oracle':
+                from airflow.hooks.oracle_hook import OracleHook
+                return OracleHook(oracle_conn_id=self.conn_id)
+            elif self.conn_type == 'vertica':
+                from airflow.contrib.hooks.vertica_hook import VerticaHook
+                return VerticaHook(vertica_conn_id=self.conn_id)
+            elif self.conn_type == 'cloudant':
+                from airflow.contrib.hooks.cloudant_hook import CloudantHook
+                return CloudantHook(cloudant_conn_id=self.conn_id)
+            elif self.conn_type == 'jira':
+                from airflow.contrib.hooks.jira_hook import JiraHook
+                return JiraHook(jira_conn_id=self.conn_id)
+            elif self.conn_type == 'redis':
+                from airflow.contrib.hooks.redis_hook import RedisHook
+                return RedisHook(redis_conn_id=self.conn_id)
+            elif self.conn_type == 'wasb':
+                from airflow.contrib.hooks.wasb_hook import WasbHook
+                return WasbHook(wasb_conn_id=self.conn_id)
+            elif self.conn_type == 'docker':
+                from airflow.hooks.docker_hook import DockerHook
+                return DockerHook(docker_conn_id=self.conn_id)
+        except:
+            pass
+
+    def __repr__(self):
+        return self.conn_id
+
+    @property
+    def extra_dejson(self):
+        """Returns the extra property by deserializing json."""
+        obj = {}
+        if self.extra:
+            try:
+                obj = json.loads(self.extra)
+            except Exception as e:
+                self.log.exception(e)
+                self.log.error("Failed parsing the json for conn_id %s", self.conn_id)
+
+        return obj
