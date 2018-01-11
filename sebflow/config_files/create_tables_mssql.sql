d782bda682d7308e72b24262a54a90237c5bc155
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-11 16:13:16 -0700

    added connection

--- sebflow/config_files/create_tables_mssql.sql
+++ sebflow/config_files/create_tables_mssql.sql
@@ -34,3 +34,21 @@ CREATE TABLE sebflow.dbo.task (
   , PRIMARY KEY (id)
   , UNIQUE (task_id, dag_run_id)
 );
+
+
+IF OBJECT_ID('sebflow.dbo.connection') IS NOT NULL DROP TABLE sebflow.dbo.connection;
+
+CREATE TABLE sebflow.dbo.connection(
+  id INT IDENTITY(1,1)
+  , conn_id NVARCHAR(50)
+  , conn_type NVARCHAR(100)
+  , host NVARCHAR(100)
+  , _schema NVARCHAR(100)
+  , login NVARCHAR(100)
+  , password NVARCHAR(100)
+  , port INTEGER
+  , is_encrypted BIT DEFAULT 0
+  , is_extra_encrypted BIT DEFAULT 0
+  , extra NVARCHAR(500)
+  , PRIMARY KEY (id)
+)
